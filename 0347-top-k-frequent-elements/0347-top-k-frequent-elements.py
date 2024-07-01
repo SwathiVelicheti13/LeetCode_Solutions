3fe7daaeaa24b61825ef51a384d38591fd435623
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-07-01 17:11:37 -0400

    Time: 138 ms (5.1%), Space: 20.8 MB (91.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,6 +6,12 @@ class Solution:
             dict1[val]+=1
         
         list1 = []
+
+
+        if len(dict1) == k:
+            return list(dict1.keys())
+
+        k = k%len(dict1)
         for a in range(k):
             max1 = 0
             for x,y in dict1.items():
