0bce25ee9690a36db68523aa34b4f2bb32e14665
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-07-01 15:06:25 -0400

    Time: 229 ms (52.57%), Space: 19.5 MB (56.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        preDict={0:1}
-        n = len(nums)
-        count = 0
         pfSum = 0
-        for val in nums:
-            pfSum+=val
-            if pfSum-k in preDict:
-                count+=preDict[pfSum-k]
-
-            if pfSum not in preDict:
-                preDict[pfSum] = 1
+        dict1 = {0:1}
+        count = 0
+        for i in range(len(nums)):
+            pfSum+=nums[i]
+            rem = pfSum-k
+            if rem in dict1:
+                count+=dict1[rem]
+            if pfSum not in dict1:
+                dict1[pfSum] = 1
             else:
-                preDict[pfSum] =  preDict[pfSum]+1
+                dict1[pfSum]+=1
         return count
-        
+
+    
