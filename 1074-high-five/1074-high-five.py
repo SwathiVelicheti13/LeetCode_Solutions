bfe34260868a56ddada83a749befb7a5b600d4d3
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-03 17:22:41 -0500

    Time: 8 ms (15.38%), Space: 16.9 MB (78.87%) - LeetHub

---
+++ 1074-high-five/1074-high-five.py
@@ -0,0 +1,18 @@
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        dict1 = defaultdict(list)
+
+        for list1 in items:
+            dict1[list1[0]].append(list1[1])
+            dict1[list1[0]].sort(reverse = True)
+
+        l1 = []
+        for key, values in dict1.items():
+            sum1=floor((sum(values[0:5]))/5)
+            l1.append([key,sum1])
+        l1.sort()
+        return l1
+
+            
+
+        
