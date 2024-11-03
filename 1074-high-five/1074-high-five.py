970d2867ce692a9d801478c5cc2c6555adf8e588
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-03 17:26:31 -0500

    Time: 0 ms (100%), Space: 16.9 MB (47.32%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -4,11 +4,11 @@ class Solution:
 
         for list1 in items:
             dict1[list1[0]].append(list1[1])
-            dict1[list1[0]].sort(reverse = True)
 
         l1 = []
         for key, values in dict1.items():
-            sum1=(sum(values[0:5]))//5
+            values = sorted(values,reverse=True)[:5]
+            sum1=sum(values)//len(values)
             l1.append([key,sum1])
         l1.sort()
         return l1
