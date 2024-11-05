d58dc56d021cb6a617f38710619ccd5a6ceac26d
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-04 20:29:44 -0500

    Time: 667 ms (33.37%), Space: 19.8 MB (88.47%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res=[]
-        for i in range(len(nums)):
-            if i!=0 and nums[i] == nums[i-1]:
+        triplets =[]
+        l = r = k = 0
+        for l in range(len(nums)):
+            if l!=0 and nums[l] == nums[l-1]:
                 continue
-            
-            j = i+1
+            r = l+1
             k = len(nums)-1
-            while j<k:
-                sum1 = nums[i]+nums[j]+nums[k]
-                if sum1<0:
-                    j+=1
-                elif sum1>0:
-                    k= k-1
-                else:
-                    temp = [nums[i],nums[j],nums[k]]
-                    res.append(temp)
-                    j = j+1
+            while r<k:
+                tripSum = nums[l]+nums[r]+nums[k]
+                if tripSum == 0:
+                    triplets.append([nums[l],nums[r],nums[k]])
+                    r+=1
                     k = k-1
-                    while j<k and nums[j] == nums[j-1]:
-                        j = j+1
-                    while j<k and nums[k] == nums[k+1]:
+                    while r<k and nums[r] == nums[r-1]:
+                        r+=1
+            
+                    while k<len(nums)-1 and r<k and nums[k] == nums[k+1] :
                         k = k-1
+                elif tripSum<0:
+                    r+=1
+                else:
+                    k= k-1
                 
-        return res
+        return triplets
+
+
+                    
+
+        
