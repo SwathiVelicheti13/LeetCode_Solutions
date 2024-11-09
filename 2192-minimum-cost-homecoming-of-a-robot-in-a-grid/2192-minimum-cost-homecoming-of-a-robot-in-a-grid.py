48db3962ae0ff38a40664fa968a10ddb1810bf19
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-08 19:32:52 -0500

    Time: 14 ms (70.33%), Space: 29.4 MB (60.38%) - LeetHub

--- 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/2192-minimum-cost-homecoming-of-a-robot-in-a-grid.py
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/2192-minimum-cost-homecoming-of-a-robot-in-a-grid.py
@@ -2,25 +2,19 @@ class Solution:
     def minCost(self, startPos: List[int], homePos: List[int], rowCosts: List[int], colCosts: List[int]) -> int:
         total = 0
 
-        if startPos[0]!=homePos[0]:
-            if startPos[0]<homePos[0]:
-                for i in range(startPos[0]+1,homePos[0]+1):
-                    total+=rowCosts[i]
-            else:
-                for i in range(startPos[0]-1,homePos[0]-1,-1):
-                    total+=rowCosts[i]
+        if startPos[0]<homePos[0]:
+            for i in range(startPos[0]+1,homePos[0]+1):
+                total+=rowCosts[i]
         else:
-            pass
-        if startPos[1]!=homePos[1]:
-            if startPos[1]<homePos[1]:
-                for j in range(startPos[1]+1,homePos[1]+1):
-                    print(j)
-                    total+=colCosts[j]
-            else:
-                for j in range(startPos[1]-1,homePos[1]-1,-1):
+            for i in range(startPos[0]-1,homePos[0]-1,-1):
+                total+=rowCosts[i]
+       
+        if startPos[1]<homePos[1]:
+            for j in range(startPos[1]+1,homePos[1]+1):
                     total+=colCosts[j]
         else:
-            pass
+            for j in range(startPos[1]-1,homePos[1]-1,-1):
+                    total+=colCosts[j]
         return total
             
 
