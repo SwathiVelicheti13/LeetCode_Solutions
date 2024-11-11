a0012ea9d5ff12a4c93912633978f12f284cd5d8
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-11 08:35:50 -0500

    Time: 135 ms (96.44%), Space: 62.2 MB (16.46%) - LeetHub

---
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        pfSum = 0
+        pfDict = {0:-1}
+        if k == 0 and len(nums) == 1:
+            return 0
+        maxLength = float('-inf')
+        for x in range(len(nums)):
+            pfSum+=nums[x]
+            Needed = pfSum-k
+            if Needed in pfDict:
+                length = (x-pfDict[Needed])
+                maxLength = max(length,maxLength)
+            if pfSum not in pfDict:
+                pfDict[pfSum] = x
+        if maxLength == float('-inf'):
+            return 0
+        return maxLength
+
+
+        
