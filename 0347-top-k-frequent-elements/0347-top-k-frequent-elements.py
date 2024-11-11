9fe54a7efe5e5621752cb865a316dee9cafde61b
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2024-11-11 13:25:39 -0500

    Time: 11 ms (25.58%), Space: 20.2 MB (82.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,20 +7,32 @@ class Solution:
         
         list1 = []
 
+        for key,val in dict1.items():
+            list1.append([-val,key])
 
-        if len(dict1) == k:
-            return list(dict1.keys())
+        heapq.heapify(list1)
 
-        k = k%len(dict1)
-        for a in range(k):
-            max1 = 0
-            for x,y in dict1.items():
-                if max1<y:
-                    max1 = y
-                    temp =x 
-            list1.append(temp)
-            dict1[temp] = 0
-        return list1
+        res = []
+        for i in range(k):
+            val,key = heapq.heappop(list1)
+            res.append(key)
+        return res
+
+
+
+        # if len(dict1) == k:
+        #     return list(dict1.keys())
+
+        # k = k%len(dict1)
+        # for a in range(k):
+        #     max1 = 0
+        #     for x,y in dict1.items():
+        #         if max1<y:
+        #             max1 = y
+        #             temp =x 
+        #     list1.append(temp)
+        #     dict1[temp] = 0
+        # return list1
                 
 
     
