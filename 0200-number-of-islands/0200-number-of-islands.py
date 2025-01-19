0481bd7246c5dca5a64508e844884922679fbdc3
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-19 07:28:38 -0500

    Time: 291 ms (18.75%), Space: 25.5 MB (19.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,33 @@
 class Solution:
-    def bfs(self,i,j,grid,n,m,visited):
-        visited[i][j] = 1
-        q = [[i,j]]
-        direction = [(0,1),(0,-1),(1,0),(-1,0)]
-        while q:
-            r, c = q.pop(0)
-            for dire in direction:
-                nr = r+dire[0]
-                nc = c+dire[1]
 
-                if nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc]=='1' and visited[nr][nc]!=1:
-                    visited[nr][nc] = 1
-                    q.append([nr,nc])
-            
+    def dfs(self,grid,r,c,visited):
+        if r<0 or c<0 or r>=len(grid) or c>=len(grid[0]) or grid[r][c]=='0':
+            return 
+
+        direc = [(0,1),(1,0),(0,-1),(-1,0)]
+
+        for dr, dc in direc:
+            nr = r+dr
+            nc = c+dc
+
+            if nr>=0 and nc>=0 and nr<len(grid) and nc<len(grid[0]) and (nr,nc) not in visited and grid[nr][nc] == '1':
+                visited.add((nr,nc))
+                self.dfs(grid,nr,nc,visited)
+    
                 
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
+
+        visited = set()
+
         cnt = 0
-        visited = [[0 for _ in range(m)]for _ in range(n)]
 
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == '1' and visited[i][j]!=1:
+                if grid[i][j] == '1' and (i,j) not in visited:
                     cnt+=1
-                    self.bfs(i,j,grid,n,m,visited)
+                    visited.add((i,j))
+                    self.dfs(grid,i,j,visited)
         return cnt
-
-
-      
+       
