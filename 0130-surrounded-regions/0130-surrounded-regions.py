3790306ee7366a9a6a1ef36d0ca288df32061091
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-19 06:23:09 -0500

    Time: 6 ms (66.4%), Space: 21.7 MB (41.8%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -7,7 +7,7 @@ class Solution:
 
         if not board or not board[0]:
             return
-
+        
         n, m = len(board), len(board[0])
         q = deque()
 
@@ -23,24 +23,28 @@ class Solution:
             if board[n - 1][j] == 'O':
                 q.append((n - 1, j))
 
-        # Mark all boundary-connected 'O' cells as safe
+        # Direction array for moving up, down, left, right
+        direc = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        visited = set()  # Keep track of visited cells
+
         while q:
             r, c = q.popleft()
-            if 0 <= r < n and 0 <= c < m and board[r][c] == 'O':
-                board[r][c] = 'S'  # Mark as safe
-                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                    q.append((r + dr, c + dc))
 
-        # Replace all 'O' with 'X' and 'S' back to 'O'
+            # If already visited, continue
+            if (r, c) in visited:
+                continue
+            visited.add((r, c))
+
+            # For every 'O' connected to the border, mark it as visited
+            for dr, dc in direc:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 'O':
+                    q.append((nr, nc))
+
+        # Now change the board, replace unvisited 'O' with 'X' and visited 'O' (safe) back to 'O'
         for i in range(n):
             for j in range(m):
-                if board[i][j] == 'O':
+                if board[i][j] == 'O' and (i, j) not in visited:
                     board[i][j] = 'X'
-                elif board[i][j] == 'S':
+                elif board[i][j] == 'O' and (i, j) in visited:
                     board[i][j] = 'O'
-
-
-
-        
-
-        
