75a09f1f71024dcb5f4af75307b1244d0d1fdad8
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-19 18:10:17 -0500

    Time: 3 ms (35.09%), Space: 18.2 MB (8.37%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1:
-            return [nums[:]]
-
         res = []
+        def helper(nums,ans,visited):
+            if len(ans) == len(nums):  # Generate permutations of size 3
+                res.append(ans[:])  # Append a copy of the current permutation
+                return
 
-        for _ in range(len(nums)):
-            n = nums.pop(0)
+            for i in range(len(nums)):
+                if not visited[i]:  # If the element is not already used
+                    visited[i] = True  # Mark it as used
+                    ans.append(nums[i])  # Include it in the current permutation
+                    helper(nums, ans, visited)  # Recurse to build further
+                    ans.pop()  # Backtrack: Remove the element
+                    visited[i] = False  # Unmark the element
 
-            perms = self.permute(nums)
-
-            for p in perms:
-                p.append(n)
-
-            res.extend(perms)
-
-            nums.append(n)
-        return res        
+        visited = [False] * len(nums)
+        helper(nums, [], visited)
+        print(res) 
+        return res
