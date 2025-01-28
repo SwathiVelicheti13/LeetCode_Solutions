6bfb2a98d51761a9158b3c1c2bc0ce61d48fd564
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-28 09:53:15 -0500

    Time: 24 ms (90.91%), Space: 20.2 MB (72.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,17 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        pfDict = {0:1}
+        totalSubArrays = 0
         pfSum = 0
-        dict1 = {0:1}
-        count = 0
+
         for i in range(len(nums)):
             pfSum+=nums[i]
-            rem = pfSum-k
-            if rem in dict1:
-                count+=dict1[rem]
-            if pfSum not in dict1:
-                dict1[pfSum] = 1
-            else:
-                dict1[pfSum]+=1
-        return count
+            Needed = pfSum-k
 
-    
+            if Needed in pfDict:
+                totalSubArrays+=pfDict[Needed]
+            
+            if pfSum not in pfDict:
+                pfDict[pfSum] = 1
+            else:
+                 pfDict[pfSum]+= 1
+        return totalSubArrays
