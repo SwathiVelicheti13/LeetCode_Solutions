edf1e6e004bb46c930ffea7ec2655757a9483bf2
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 14:47:06 -0500

    Time: 324 ms (25.5%), Space: 66.7 MB (14.57%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -3,6 +3,17 @@ class Solution:
         intervals = sorted(zip(startTime,endTime,profit))
         dp = {}
         start_times = [job[0] for job in intervals]
+
+        def binary_search(target):
+            left, right = 0, len(start_times) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if start_times[mid] >= target:
+                    right = mid - 1  # Move left to find the first valid index
+                else:
+                    left = mid + 1  # Move right to increase start time
+            return left
+
         def jobProfit(ind):
             if ind == len(intervals):
                 return 0
@@ -19,8 +30,9 @@ class Solution:
             #         break
             #     j+=1
 
-            j = bisect_left(start_times, intervals[ind][1])
+            # j = bisect_left(start_times, intervals[ind][1])
             
+            j = binary_search(intervals[ind][1])
             dp[ind] =  max(res, intervals[ind][2]+jobProfit(j))
             return dp[ind]
         
