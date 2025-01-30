a84a395cba11f9ec748d979dcf400810b5d135ec
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 14:42:06 -0500

    Time: 175 ms (63.58%), Space: 67 MB (14.17%) - LeetHub

---
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -0,0 +1,27 @@
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        intervals = sorted(zip(startTime,endTime,profit))
+        dp = {}
+        start_times = [job[0] for job in intervals]
+        def jobProfit(ind):
+            if ind == len(intervals):
+                return 0
+
+            if ind in dp:
+                return dp[ind]
+            res = jobProfit(ind+1)
+
+            # while loop for next non overlapping interval
+
+            # j = ind+1
+            # while j<len(intervals):
+            #     if intervals[ind][1]<=intervals[j][0]:
+            #         break
+            #     j+=1
+
+            j = bisect_left(start_times, intervals[ind][1])
+            
+            dp[ind] =  max(res, intervals[ind][2]+jobProfit(j))
+            return dp[ind]
+        
+        return jobProfit(0)
