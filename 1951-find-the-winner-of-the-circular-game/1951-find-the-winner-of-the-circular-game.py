7d76ae088f9905ee1c31a8fcab471a7905c67748
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 10:17:16 -0500

    Time: 3 ms (57.74%), Space: 17.9 MB (32.9%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,8 +1,11 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        people = list(range(1, n + 1))
-        idx = 0
-        while len(people) > 1:
-            idx = (idx + (k - 1)) % len(people)
-            people.pop(idx)
+        people = list(range(1,(n+1)))
+        ind = 0
+
+        while len(people)>1:
+            ind = (ind+(k-1))%len(people)
+            people.pop(ind)
+
         return people[0]
+
