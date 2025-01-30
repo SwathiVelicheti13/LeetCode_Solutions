4c2b98a3d095af576afed4e928251f9f4a85da0b
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 18:29:02 -0500

    Time: 310 ms (23.58%), Space: 28.5 MB (24.76%) - LeetHub

--- 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
@@ -2,10 +2,11 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         nums.sort()  # Sort the numbers to work with the smallest ones first
         l = 0
+        r = 0
         sum1 = 0
         ans = 0
         
-        for r in range(len(nums)):
+        while r<len(nums):
             sum1 += nums[r]
             
             # Check if we can make all elements between nums[l] and nums[r] the same with k operations
@@ -15,6 +16,7 @@ class Solution:
             
             # Update the result with the largest window size
             ans = max(ans, r - l + 1)
+            r+=1
         
         return ans
 
