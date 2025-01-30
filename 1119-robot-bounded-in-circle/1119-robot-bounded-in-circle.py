a56b24d437b10a57536a3bf0bee831b22e260585
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 09:51:10 -0500

    Time: 1 ms (20.75%), Space: 17.7 MB (54.54%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.py
@@ -2,22 +2,18 @@ class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         x = 0 
         y = 0
+        dir_ind = 0
 
-        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        direc = [(0,1),(1,0),(0,-1),(-1,0)]
 
-        d = 0
 
         for ins in instructions:
             if ins == 'G':
-                x= x + directions[d][0]
-                y= y + directions[d][1]
+                x+=direc[dir_ind][0]
+                y+=direc[dir_ind][1]
             elif ins == 'L':
-                d = (d-1)%4
+                dir_ind = (dir_ind-1)%4
             elif ins == 'R':
-                d = (d+1)%4
-        
-        return(x == 0 and y == 0) or d != 0
+                dir_ind = (dir_ind+1)%4
 
-
-        
-        
+        return (x==0 and y==0) or (dir_ind!=0)
