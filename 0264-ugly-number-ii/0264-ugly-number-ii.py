03f11acf1977ca0958b2ebf9c4057aac39cad9ab
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 20:11:30 -0500

    Time: 79 ms (54.66%), Space: 17.8 MB (43.39%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,39 +1,39 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
 
-        # ugly = [1] * n  # Array to store ugly numbers
-        # p2 = p3 = p5 = 0  # Pointers for multiples of 2, 3, and 5
-        
-        # for i in range(1, n):
-        #     next_ugly = min(ugly[p2] * 2, ugly[p3] * 3, ugly[p5] * 5)  # Find next ugly number
-        #     ugly[i] = next_ugly  # Store the next ugly number
-            
-        #     # Increment the pointers for which the value was used
+        primes = [2,3,5]
+        uglyHeap = [1]
+        heapq.heapify(uglyHeap)
+        visited = set()
+        visited.add(1)
+
+        for _ in range(n):
+            uglyNum = heapq.heappop(uglyHeap)
+            for x in primes:
+                new_ugly = uglyNum*x
+                if new_ugly not in visited:
+                    heapq.heappush(uglyHeap,new_ugly)
+                    visited.add(new_ugly)
+        return uglyNum
+
+        # O(N) approach
+
+        # ugly = [1] * n
+        # p2 = p3 = p5 = 0
+
+        # for i in range(1,n):
+        #     next_ugly = min(ugly[p2] * 2,ugly[p3] * 3,ugly[p5] * 5)
+        #     ugly[i] = next_ugly
+
         #     if next_ugly == ugly[p2] * 2:
-        #         p2 += 1
+        #         p2+=1
         #     if next_ugly == ugly[p3] * 3:
-        #         p3 += 1
+        #         p3+=1
         #     if next_ugly == ugly[p5] * 5:
-        #         p5 += 1
-                
-        # return ugly[-1]  # Return the nth ugly number
-
-        ugly = [1] * n
-        p2 = p3 = p5 = 0
-
-        for i in range(1,n):
-            next_ugly = min(ugly[p2] * 2,ugly[p3] * 3,ugly[p5] * 5)
-            ugly[i] = next_ugly
-
-            if next_ugly == ugly[p2] * 2:
-                p2+=1
-            if next_ugly == ugly[p3] * 3:
-                p3+=1
-            if next_ugly == ugly[p5] * 5:
-                p5+=1
+        #         p5+=1
             
-        return ugly[-1]
-        
+        # return ugly[-1]
+
         # primes = [2,3,5]
         # uglyHeap = [1]
         # heapq.heapify(uglyHeap)
