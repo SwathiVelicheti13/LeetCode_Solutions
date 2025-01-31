1fd6c489db45a953624559e0d3cafdc1d970e965
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 21:09:19 -0500

    Time: 83 ms (84.96%), Space: 21.4 MB (26.53%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -1,31 +1,25 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        arr = nums
-        totalSum = 0    
-        currMaxSum = 0
-        currMinSum = 0
-        maxSum = arr[0]
-        minSum = arr[0]
-        
-        for i in range(len(arr)):
-        
-            # Kadane's to find maximum sum subarray
-            currMaxSum = max(currMaxSum + arr[i], arr[i])
-            maxSum = max(maxSum, currMaxSum) 
+        currMaxsum = nums[0]
+        currMinsum = nums[0]
+
+        maxSum = nums[0]
+        minSum = nums[0]
+        total = nums[0]
+        for i in range(1,len(nums)):
+            if currMaxsum+nums[i]<nums[i]:
+                currMaxsum = nums[i]
+            else:
+                currMaxsum+=nums[i]
+            maxSum = max(maxSum,currMaxsum)
+
+            if currMinsum+nums[i]>nums[i]:
+                currMinsum = nums[i]
+            else:
+                currMinsum+=nums[i]
             
-            # Kadane's to find minimum sum subarray
-            currMinSum = min(currMinSum + arr[i], arr[i])
-            minSum = min(minSum, currMinSum)
-            
-            # Sum of all the elements of input array
-            totalSum += arr[i]
-        
-        normalSum = maxSum
-        circularSum = totalSum - minSum
-        
-        # If the minimum subarray is equal to total Sum
-        # then we just need to return normalSum
-        if minSum == totalSum:
-            return normalSum
-        
-        return max(normalSum, circularSum)
+            minSum = min(minSum,currMinsum)
+            total+=nums[i]
+        if minSum == total:
+            return maxSum
+        return max(maxSum, total-minSum)
