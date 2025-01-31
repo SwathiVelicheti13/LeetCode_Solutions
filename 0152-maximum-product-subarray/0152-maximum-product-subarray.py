cf39b84d51b11adc8974a5250daa2631fc2bf6eb
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 22:00:41 -0500

    Time: 7 ms (90.9%), Space: 18.4 MB (30.34%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,22 +3,17 @@ class Solution:
 
         if not nums:
             return 0
+
         currMax = currMin = maxProd = nums[0]
 
         for i in range(1,len(nums)):
-            num = nums[i]
+            num =nums[i]
 
-            if num < 0:
-                currMax, currMin = currMin, currMax
+            if num<0:
+                currMin,currMax = currMax,currMin
 
-            currMax = max(num,currMax * num)
-            currMin = min(num,currMin * num)
+            currMax = max(num,num*currMax)
+            currMin = min(num,num*currMin)
 
             maxProd = max(maxProd,currMax)
         return maxProd
-
-
-
-
-
-       
