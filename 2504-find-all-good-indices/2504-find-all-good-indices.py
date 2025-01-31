c154e9ec5232de2d1bbe3a979d65a2396a08a9f7
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-01-30 22:13:52 -0500

    Time: 113 ms (48.39%), Space: 36.4 MB (47.47%) - LeetHub

--- 2504-find-all-good-indices/2504-find-all-good-indices.py
+++ 2504-find-all-good-indices/2504-find-all-good-indices.py
@@ -1,68 +1,22 @@
 class Solution:
     def goodIndices(self, nums: List[int], k: int) -> List[int]:
+        left = [1]*len(nums)
+        right = [1]*len(nums)
 
-        n = len(nums)
+        result = []
 
-        ans = []
+        for i in range(1,len(left)):
+            if nums[i]<=nums[i-1]:
+                left[i]=left[i-1]+1
+        
+        for j in range(len(right)-2,-1,-1):
+            if nums[j]<=nums[j+1]:
+                right[j]=right[j+1]+1
+        
+        for x in range(k,len(nums)-k):
+            if left[x-1] >=k and right[x+1]>=k:
+                result.append(x)
+        return result
 
-        if n == 3:
-
-            return [1]
-
-        if k == 1:
-
-            for i in range(1, n - 1):
-
-                ans.append(i)
-
-            return ans
-
-        left = set()
-
-        right = set()
-
-        for i in range (k - 1):
-
-            if nums[i] < nums[i + 1]:
-
-                left.add(i)
-
-        for i in range(k + 1, 2 * k):
-
-            if i + 1 >= n:
-                
-                break
-
-            if nums[i] > nums[i + 1]:
-
-                right.add(i)
-
-        for i in range(k, n - k + 1):
-
-            if i + k >= n:
-
-                break
-
-            if len(left) == 0 and len(right) == 0:
-
-                ans.append(i)
-
-            if i - k in left:
-
-                left.remove(i - k)
-
-            if i + 1 in right:
-
-                right.remove(i + 1)
-
-            if nums[i] > nums[i - 1]:
-
-                left.add(i - 1)
-
-            if i + k + 1 < n and nums[i + k] > nums[i + k + 1]:
-
-                right.add(i + k)
-
-            
-        return ans
-      
+        
+        
