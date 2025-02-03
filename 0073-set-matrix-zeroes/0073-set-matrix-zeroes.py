95a4bac11e5db290ae4d07fbb329d141de0e31dd
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-02-03 07:46:05 -0500

    Time: 7 ms (22.09%), Space: 18.4 MB (47.17%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,22 +4,58 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
 
-        rows = len(matrix)
-        cols = len(matrix[0])
-        rowSet = set()
-        colSet = set()
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j] == 0:
-                    rowSet.add(i)
-                    colSet.add(j)
-        
+        n = len(matrix)
+        m = len(matrix[0])
 
-        for i in range(rows):
-            for j in range(cols):
-                if i in rowSet or j in colSet:
+        rowZero = any(matrix[0][j] == 0 for j in range(m))
+        colZero = any(matrix[i][0]== 0 for i in range(n))
+       
+
+        # marking first rows and columns as zeroes
+        for r in range(1,n):
+            for c in range(1,m):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+
+        # use that markings to mark other rows and columns to zero
+
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
-        return matrix
+        if rowZero:
+            for c in range(m):
+                matrix[0][c] = 0
+        
+        if colZero:
+            for r in range(n):
+                matrix[r][0] = 0
 
         
+
+
+        
+
+
+        # rows = len(matrix)
+        # cols = len(matrix[0])
+        # rowSet = set()
+        # colSet = set()
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if matrix[i][j] == 0:
+        #             rowSet.add(i)
+        #             colSet.add(j)
+        
+
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if i in rowSet or j in colSet:
+        #             matrix[i][j] = 0
+
+        # return matrix
+
+    
+       
