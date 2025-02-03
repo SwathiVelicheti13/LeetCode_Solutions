ec7f8be20d7f3b47a40b7ad77e91b924be972b49
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-02-03 09:21:51 -0500

    Time: 131 ms (64.52%), Space: 32.9 MB (74.49%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,29 +1,34 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        n = len(matrix)
-        m = len(matrix[0])
-        dp = [[0 for _ in range(m)] for _ in range(n)]
-        maxarea = 0
-        for i in range(m):
-            dp[0][i]= int(matrix[0][i])
         
-        for j in range(n):
-            dp[j][0] = int(matrix[j][0])
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        for i in range(1,n):
-            for j in range(1,m):
-                if matrix[i][j] == "0":
-                    print(matrix[i][j],i,j)
-                    dp[i][j] = 0
+        dp = [[0 for _ in range(cols)]for _ in range(rows)]
+        maxside = 0
+
+        for i in range(rows):
+            dp[i][0] = int(matrix[i][0])
+            maxside = max(maxside,dp[i][0])
+
+        for j in range(cols):
+            dp[0][j] = int(matrix[0][j])
+            maxside = max(maxside,dp[0][j])
+        
+
+        for i in range(1,rows):
+            for j in range(1,cols):
+                if matrix[i][j] == '0':
+                    dp[i][j]= 0
                 else:
-                    dp[i][j] = min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1
+                    dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                    maxside = max(maxside,dp[i][j])
+    
+        return maxside*maxside
 
-        maxarea = 0
 
-        for i in range(n):
-            for j in range(m):
-                maxarea = max(maxarea,dp[i][j])
-        maxarea = int(math.pow(maxarea,2))
-        return maxarea
 
+            
+            
+        
         
