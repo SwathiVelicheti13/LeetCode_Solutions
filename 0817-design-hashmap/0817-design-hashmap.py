4d551f7f64c76a8504fbe71ab9ba6775d635dc02
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-02-04 04:36:40 -0500

    Time: 36 ms (72.49%), Space: 20.7 MB (89.36%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,20 +1,25 @@
 class MyHashMap:
 
     def __init__(self):
-        self.MyHashMap = {}
+        self.hash_map={}
+      
         
     def put(self, key: int, value: int) -> None:
-        self.MyHashMap[key] = value
-        
+        if key not in self.hash_map:
+            self.hash_map[key] = value
+        else:
+            self.hash_map[key] = value
 
     def get(self, key: int) -> int:
-        if len(self.MyHashMap)>0 and key in self.MyHashMap:
-            return self.MyHashMap[key]
+        if key in self.hash_map:
+            return self.hash_map[key]
         return -1
 
     def remove(self, key: int) -> None:
-        if len(self.MyHashMap)>0 and key in self.MyHashMap:
-            self.MyHashMap.pop(key)
+        if key in self.hash_map:
+            del self.hash_map[key]
+        return -1
+       
         
 
 
