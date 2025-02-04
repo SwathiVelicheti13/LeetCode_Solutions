fb5fa7b0fdac774e33e4c8a9ee7ed0e33bcb4a77
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-02-04 06:15:42 -0500

    Time: 7 ms (89.07%), Space: 19.2 MB (60.88%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,29 +1,27 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        lmax = 0
-        rmax = 0 
-        l = 0
-        r = len(height)-1
-        totalmax = 0
-
-        while l<=r:
-            if height[l]<=height[r]:
-                if height[l]<=lmax:
-                    totalmax+=(lmax-height[l])
+        leftmax = 0 
+        rightmax = 0
+        left_ptr = 0
+        right_ptr = len(height)-1
+        trapped_water = 0
+        while left_ptr<=right_ptr:
+            # compare heights at left and right pointers
+            if height[left_ptr]<=height[right_ptr]:
+                #checking and updating max left bar
+                if height[left_ptr]>=leftmax:
+                    leftmax = height[left_ptr]
                 else:
-                    lmax = height[l]
-                l+=1
+                    trapped_water+=(leftmax-height[left_ptr])
+                left_ptr+=1
             else:
-                if height[r]<=rmax:
-                    totalmax+=(rmax-height[r])
+                if height[right_ptr]>=rightmax:
+                    rightmax = height[right_ptr]
                 else:
-                    rmax = height[r]
-                r = r-1
-        return totalmax    
+                    trapped_water+=(rightmax-height[right_ptr])
+                right_ptr= right_ptr-1
+        return trapped_water
 
-                    
-         
-            
 
-                    
-         
+
+        
