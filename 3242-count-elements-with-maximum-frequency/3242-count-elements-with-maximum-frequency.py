4b1ac64fe1d286394e64db858124cfc02d50fc65
Swathi Velicheti <74660087+SwathiVelicheti13@users.noreply.github.com>
2025-10-04 14:35:28 -0400

    Time: 7 ms (18.41%), Space: 12.5 MB (10.33%) - LeetHub

---
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -0,0 +1,27 @@
+from collections import Counter 
+
+class Solution(object):
+    def maxFrequencyElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        c = Counter(nums)
+        print(c)
+
+        c = list(c.values())
+        print(c)
+
+        max_number = max(c)
+        result = 0
+
+        for i in c:
+            if i>max_number:
+                break
+            elif i==max_number:
+                result = i+result
+        return result
+
+
+
+        
